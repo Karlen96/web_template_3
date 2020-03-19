e071c34bd5c1fbe83214f972356f9d0199c79c03
KarenMkrtchyan <KarenMkrtchyan@users.noreply.github.com>
2020-03-19 18:26:39 +0400

    part 2

--- script.js
+++ script.js
@@ -122,14 +122,24 @@ let counterStatisticCoffee = document.querySelector(".countCoffee");
 let counterStatisticPosts = document.querySelector(".countPosts");
 let counterStatisticLikes = document.querySelector(".countLikes");
 let counterStatisticLaunched = document.querySelector(".countLaunched");
-let countStartif = 0, countClients = 0, countCoffee = 0, countPosts = 0, countLikes = 0, countLaunched = 0;
+let countStartStatisticCount = 0,
+	countStartStatisticFrameworks = 0,
+	countClients = 0,
+	countCoffee = 0,
+	countPosts = 0,
+	countLikes = 0,
+	countLaunched = 0,
+	countJqueryHeight = 0,
+	countVueHeight = 0,
+	countReactHeight = 0,
+	countAngularHeight = 0;
 
 let scrollUpPage = document.querySelector(".scrollup");
 
 window.onscroll = function () {
-
-	if (countStartif == 0 && pageYOffset > 2450) {
-		countStartif = 1;
+	/*scrollup================================================*/
+	if (countStartStatisticCount == 0 && pageYOffset > 2450) {
+		countStartStatisticCount = 1;
 		let count1 = setInterval(function counter1() {
 			counterStatisticClients.innerHTML = countClients++;
 			if (countClients >= counterStatisticClients.getAttribute("data_count")) {
@@ -166,18 +176,53 @@ window.onscroll = function () {
 		}, 10);
 	}
 
-
-	/*scrollup================================================*/
-	if (pageYOffset > 1000) {
-		scrollUpPage.style.display = "flex";
-		scrollUpPage.onclick = function scrollUp() {
-			window.scroll(0, 0);
-		}
-	} else {
-		scrollUpPage.style.display = "none";
-	}
 	/*/scrollup================================================*/
 
+
+	/*statistic__frameworks================================================*/
+	let jqueryStatisticBlock = document.querySelector(".jquery__statistic_block");
+	let vueStatisticBlock = document.querySelector(".vue__statistic_block");
+	let reactStatisticBlock = document.querySelector(".react__statistic_block");
+	let angularStatisticBlock = document.querySelector(".angular__statistic_block");
+
+	if (pageYOffset > 3700 && countStartStatisticFrameworks == 0) {
+		let countJquery = setInterval(function counterJquery() {
+			jqueryStatisticBlock.style.height = `${countJqueryHeight++}px`;
+			jqueryStatisticBlock.innerHTML = `${countJqueryHeight++}`;
+			countStartStatisticFrameworks = 1;
+			if (countJqueryHeight >= jqueryStatisticBlock.getAttribute("data_height")) {
+				clearInterval(countJquery);
+			}
+		}, 50);
+
+
+		let countVue = setInterval(function counterJquery() {
+			vueStatisticBlock.style.height = `${countVueHeight++}px`;
+			vueStatisticBlock.innerHTML = `${countVueHeight++}`;
+			if (countVueHeight >= vueStatisticBlock.getAttribute("data_height")) {
+				clearInterval(countVue);
+			}
+		}, 50);
+
+
+		let countReact = setInterval(function counterJquery() {
+			reactStatisticBlock.style.height = `${countReactHeight++}px`;
+			reactStatisticBlock.innerHTML = `${countReactHeight++}`;
+			if (countReactHeight >= reactStatisticBlock.getAttribute("data_height")) {
+				clearInterval(countReact);
+			}
+		}, 50);
+
+		let countAngular = setInterval(function counterJquery() {
+			angularStatisticBlock.style.height = `${countAngularHeight++}px`;
+			angularStatisticBlock.innerHTML = `${countAngularHeight++}`;
+			if (countAngularHeight >= angularStatisticBlock.getAttribute("data_height")) {
+				clearInterval(countAngular);
+			}
+		}, 50);
+	}
+
+	/*/statistic__frameworks================================================*/
 }
 
 /*/counter================================================*/
@@ -189,10 +234,6 @@ let search__input = document.querySelector(".search__input");
 let searchValue = "";
 const productsCardItems = document.querySelectorAll(".projects__card_bottom *");
 
-for (let i = 0; i < productsCardItems.length; i++) {
-	console.log(productsCardItems[i])
-}
-
 search__item.onmouseover = function () {
 	search__input.style.right = 35 + "px";
 }
@@ -201,7 +242,6 @@ search__input.onkeyup = function () {
 	searchValue = search__input.value;
 	for (let i = 0; i < filterItems.length; i++) {
 		if (productsCardItems[i].innerHTML == searchValue) {
-			console.log(productsCardItems[i].style.color = "red");
 		} else {
 			console.log("such a query failed");
 		}
@@ -223,12 +263,48 @@ for (let i = recentpostShowCard; i < recentpostscards.length; i++) {
 }
 read__more_btn.onclick = function readMore() {
 	recentpostShowCard += 3;
-	for (let i = 0; i < recentpostShowCard; i++) {
-		recentpostscards[i].style.display = "flex";
+	if (recentpostShowCard <= recentpostscards.length) {
+		for (let i = 0; i < recentpostShowCard; i++) {
+			recentpostscards[i].style.display = "flex";
+		}
 	}
 }
 /*/recentpostscardreadmore================================================*/
+/*reviewsstars================================================*/
+let reviewsCardStars = document.querySelectorAll(".reviews__card_stars span");
 
+for (let i = 0; i < reviewsCardStars.length; i++) {
+
+	reviewsCardStars[i].onclick = function (event) {
+		let starassessment = i;
+		if (starassessment < 5) {
+			for (let i = 0; i < 5; i++) {
+				reviewsCardStars[i].innerHTML = "&#9734;";
+			}
+			for (let i = 0; i <= starassessment; i++) {
+				reviewsCardStars[i].innerHTML = "&#9733;";
+			}
+		}
+		if (starassessment < 10) {
+			for (let i = 5; i < 10; i++) {
+				reviewsCardStars[i].innerHTML = "&#9734;";
+			}
+			for (let i = 5; i <= starassessment; i++) {
+				reviewsCardStars[i].innerHTML = "&#9733;";
+			}
+		}
+		if (starassessment < 15) {
+			for (let i = 10; i < 15; i++) {
+				reviewsCardStars[i].innerHTML = "&#9734;";
+			}
+			for (let i = 10; i <= starassessment; i++) {
+				reviewsCardStars[i].innerHTML = "&#9733;";
+			}
+		}
+	}
+}
+
+/*/reviewsstars================================================*/
 
 
 
